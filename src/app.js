cae46c0bfba250b2108741f9e60c76795b301a63
kim <kimmike19@gmail.com>
2024-04-23 18:19:19 -0400

    Integrated API to receive current forecast in real time

--- src/app.js
+++ src/app.js
@@ -53,6 +53,13 @@ function handleSearch(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "6d8ee4ta80ofbb474f648f3f30821a73";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -60,24 +67,26 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
     <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
         <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temp">
-            <strong>18°</strong></div>
-            <div class="weather-forecast-temp">12°</div>
+            <strong>${Math.round(day.temperature.maximum)}°</strong></div>
+            <div class="weather-forecast-temp">${Math.round(
+              day.temperature.minimum
+            )}°</div>
         </div>
     </div>  
-`;
+    `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
